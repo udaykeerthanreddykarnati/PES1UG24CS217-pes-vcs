54e8556a32cb55b8e49b07c412b659996bf01c01
udaykeerthanreddykarnati <udaykeerthanreddykarnati@gmail.com>
2026-04-21 19:05:47 +0530

    phase4: author part done

--- commit.c
+++ commit.c
@@ -211,6 +211,11 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         c.has_parent = 0;   // first commit in the repo
     }
 
+    // ── 3. Fill author, timestamp, and message ───────────────────────────────
+    snprintf(c.author, sizeof(c.author), "%s", pes_author());
+    c.timestamp = (uint64_t)time(NULL);
+    snprintf(c.message, sizeof(c.message), "%s", message);
+
     
 
     if (commit_id_out) *commit_id_out = commit_id;
